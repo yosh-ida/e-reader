108a099de95ea5187e5240344ce503717853cf80
yosh-ida <haru13191018@gmail.com>
2022-02-16 01:26:12 +0900

    virtualize viewer and fix key bind bug

--- src/Scene/Viewer.tsx
+++ src/Scene/Viewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import SwipeableViews from 'react-swipeable-views';
 import { bindKeyboard, SlideRenderProps, virtualize } from 'react-swipeable-views-utils';
@@ -13,15 +13,14 @@ interface ViewerLocationParam {
 function ViewerScene() {
     //  state === null => redirect 404 ?
 
-    const ExtendSwipeableViews = useMemo(() => virtualize(bindKeyboard(SwipeableViews)), []);
+    const ExtendSwipeableViews = useMemo(() => virtualize(SwipeableViews), []);
 
     const { state } = useLocation() as ViewerLocationParam;
-    // const [content, setContent] = useState<BookContent | null>(null);
     const [book, setBook] = useState<BookContent | null>(null);
     const [swipeindex, setSwipeIndex] = useState<number>(0);
     const [img, setImg] = useState<string[]>([]);
-
-    // console.log(state);
+    const [src, setSrc] = useState<{ [key: number]: string | null }>({});
+    const [page, setPage] = useState<{ pagebefore: number, pageafter: number }>({ pagebefore: 0, pageafter: 0 });
 
     useEffect(
         () => {
@@ -30,7 +29,13 @@ function ViewerScene() {
             (async () => {
                 const file = state instanceof File ? state as File : await (state as FileSystemFileHandle).getFile();
                 const cbz = await ParseCBZ(file);
+                const after = Math.min(cbz.pages - 1, 1);
+                const imgs: { [key: number]: string | null } = {};
+                for (let i = 0; i <= after; i++)
+                    imgs[i] = null;
                 setBook(cbz);
+                setPage({ pagebefore: 0, pageafter: after });
+                setSrc(imgs);
             })();
         }, []);
 
@@ -38,6 +43,7 @@ function ViewerScene() {
         () => {
             if (!book)
                 return;
+            // console.log("book updated");
             (async () => {
                 let arr: string[] = [];
                 for (let i = 0; i < book.pages; i++) {
@@ -49,36 +55,84 @@ function ViewerScene() {
         }, [book]
     );
 
+    useEffect(
+        () => {
+            if (!book)
+                return;
+            let updated = false;
+            for (let key in src) {
+                if (src[key] || +key >= img.length)
+                    continue;
+                updated = true;
+                break;
+            }
+            if (!updated)
+                return;
+            const imgs: { [key: number]: string | null } = {};
+            for (let key in src)
+                if (+key < img.length)
+                    imgs[+key] = img[key];
+            setSrc(imgs);
+        }, [book, img]
+    );
+
+    //  imgの更新に影響されたくない
     const onChangeIndex = useMemo(
         () => {
             if (!book)
-                return () => { };
+                return (index: number, indexLatest: number) => { };
             else
                 return (index: number, indexLatest: number) => {
-                    //  swipableviewのpagemaxが更新されないと
                     // console.log("page: " + indexLatest + " to " + index);
-                    if (index >= 0 && index < book.pages && (index - indexLatest === 1 || index - indexLatest === -1))
-                        setSwipeIndex(index);
-                }
-        }, [book]);
+                    const pagebefore = index === 0 ? 0 : 1;
+                    const pageafter = index === book.pages - 1 ? 0 : 1;
 
+                    const imgs: { [key: number]: string | null } = {};
+                    if (pagebefore >= 1)
+                        imgs[index - 1] = index >= 1 && index - 1 < img.length ? img[index - 1] : null;
+                    imgs[index] = index >= 0 && index < img.length ? img[index] : null;
+                    if (pageafter >= 1)
+                        imgs[index + 1] = index + 1 < img.length ? img[index + 1] : null;
+                    setSrc(imgs);
+                    setPage({ pagebefore, pageafter });
+                    setSwipeIndex(index);
+                }
+        }, [book, img]);
+    // }, []);
+
+    //  imgの更新に影響されたくない
     const slideRenderer = useMemo(
         () => ({ key, index }: SlideRenderProps) => (
-            key < img.length && img[key] ?
+            key in src && src[key] ?
                 <div key={"div" + key} className="imgwrap">
-                    <img src={img[key]} key={"img" + key} alt={"p" + key} className="imgobj" />
+                    <img src={src[key]!!} key={"img" + key} alt={"p" + key} className="imgobj" />
                 </div> :
                 <div key={"loading" + key}>loading</div>
-        ), [img]);
+        ), [src]);
 
-    // const view = useMemo(
-    //     () => <ExtendSwipeableViews index={swipeindex} overscanSlideAfter={book ? book.pages - swipeindex - 1 : 0} overscanSlideBefore={swipeindex} slideRenderer={slideRenderer} onChangeIndex={onChangeIndex} axis="x" enableMouseEvents />,
-    //     [book, swipeindex]);
+    const handleUserKeyPress = useCallback(
+        book ? ({ key, keyCode }) => {
+            if (keyCode === 39) {
+                if (swipeindex + 1 < book.pages)
+                    onChangeIndex(swipeindex + 1, swipeindex);
+            } else if (keyCode === 37 && swipeindex > 0)
+                onChangeIndex(swipeindex - 1, swipeindex);
+        } : () => { }, [book, swipeindex, onChangeIndex]);
+    useEffect(() => {
+        window.addEventListener("keydown", handleUserKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleUserKeyPress);
+        };
+    }, [handleUserKeyPress]);
+
+    const view = useMemo(
+        () => <ExtendSwipeableViews index={swipeindex} overscanSlideAfter={page.pageafter} overscanSlideBefore={page.pagebefore} slideRenderer={slideRenderer} onChangeIndex={onChangeIndex} axis="x" enableMouseEvents />,
+        [page, swipeindex, slideRenderer, onChangeIndex]);
 
     return (
-        <div style={{ width: "100%", height: "100vh" }}>
+        <div style={{ width: "100%", height: "100vh" }} onKeyDown={() => console.log("pressed")}>
             {/* {view} */}
-            <ExtendSwipeableViews index={swipeindex} overscanSlideAfter={book ? book.pages - swipeindex - 1 : 0} overscanSlideBefore={swipeindex} slideRenderer={slideRenderer} onChangeIndex={onChangeIndex} axis="x" enableMouseEvents />
+            <ExtendSwipeableViews index={swipeindex} overscanSlideAfter={page.pageafter} overscanSlideBefore={page.pagebefore} slideRenderer={slideRenderer} onChangeIndex={onChangeIndex} axis="x" enableMouseEvents />
         </div>
     );
 }
